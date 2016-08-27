525c9ca2ba07969414c96cbf71862f8bee205ce2
amit dash <dbamit@thoughtworks.com>
2016-08-27 12:47:27 +0530

    First commit of solutions

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,29 +1,36 @@
 package fpinscala.datastructures
 
-sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+sealed trait List[+A]
+
+// `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing]
+
+// A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
 which may be `Nil` or another `Cons`.
  */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+object List {
+
+  // `List` companion object. Contains functions for creating and working with lists.
+  def sum(ints: List[Int]): Int = ints match {
+    // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -34,35 +41,157 @@ object List { // `List` companion object. Contains functions for creating and wo
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, concatenate(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def printElements[A](l: List[A]): Unit =
+    l match {
+      case Nil => println()
+      case Cons(h, t) => {
+        print(h)
+        if (t != Nil) print(", ")
+        printElements(t)
+      }
+    }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => tail
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => Cons(h, tail)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => {
+        if (n > 1) drop(tail, n - 1)
+        else tail
+      }
+    }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => {
+        if (f(head)) dropWhile(tail, f)
+        else l
+      }
+    }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("Cannot remove elements from an empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(head, tail) => Cons(head, init(tail))
+    }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  def optimizedProduct(ns: List[Int]): Int = {
+    foldRight(ns, 1)((x, y) => if (x != 0) x * y else 0)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((x, y) => y + 1)
+  }
+
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+
+  def sumWithFoldLeft(l: List[Int]): Int = {
+    foldLeft(l, 0)((a, b) => a + b)
+  }
+
+  def productWithFoldLeft(l: List[Int]): Int = {
+    foldLeft(l, 1)((a, b) => a * b)
+  }
+
+  def lengthWithFoldLeft[A](l: List[A]): Int = {
+    foldLeft(l, 0)((x, y) => x + 1)
+  }
+
+  def reverse[A](l: List[A]): List[A] = {
+    foldLeft(l, List[A]())((b, a) => Cons(a, b))
+  }
+
+  //def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B
+  // as --> list
+  // z --> frZero (B => B)
+  // f --> (A, (B => B)) returns (B => B)
+  // Make sure that flFunc executes first and then frFunc executes
+  // Lastly call the function returned by fold right with acc as input
+  def foldLeftUsingFoldRight[A, B](list: List[A], acc: B)(flFunc: (B, A) => B): B = {
+    foldRight(list, (frZero: B) => frZero)((a, frFunc) => {
+      b => {
+        frFunc(flFunc(b, a))
+      }
+    })(acc)
+  }
+
+  def appendElement[A](l: List[A], n: A): List[A] = {
+    append(l, Cons(n, Nil))
+  }
+
+  def concatenate[A](l1: List[A], l2: List[A]): List[A] = {
+    foldRight(l1, l2)(Cons(_, _))
+  }
+
+  def appendElementWithFoldRight[A](l: List[A], n: A): List[A] = {
+    concatenate(l, Cons(n, Nil))
+  }
+
+  def transformByAddingOne(l: List[Int]) : List[Int] =
+  l match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(h + 1, transformByAddingOne(t))
+  }
+
+  def map[A, B](l: List[A])(f: A => B): List[B] =
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
+
+  //  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+  //  l match {
+  //    case Nil => Nil
+  //    case Cons(h, t) => Cons(f(h), map(t)(f))
+  //  }
+}
+
+object Test extends App {
+  val testList = List[Int](1, 2, 3, 4);
+  //List.printElements(List.drop(testList, 3))
+  //List.printElements(List.dropWhile(testList, (x: Int) => (x < 3)))
+  //List.printElements(List.init(testList))
+  //println(foldRight(testList, 1) (_ * _))
+  //println(List.length(testList))
+  //println(List.sumWithFoldLeft(testList))
+  //println(List.productWithFoldLeft(testList))
+  //println(List.lengthWithFoldLeft(testList))
+  //List.printElements((List.reverse(testList)))
+  //println(List.foldLeftUsingFoldRight(testList, 0)((a, b) => a + b))
+  List.printElements(List.appendElement(testList, 5))
+  //List.printElements(List.map(List[Int](1,2,3))(i => i + 1))
 }
--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -1,11 +1,16 @@
 package fpinscala.state
 
+import fpinscala.state.RNG.{Rand, Simple}
+
+import scala.runtime.Nothing$
+
 
 trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 }
 
 object RNG {
+
   // NB - this was called SimpleRNG in the book text
 
   case class Simple(seed: Long) extends RNG {
@@ -24,47 +29,144 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
+    case (n, newRng) if n == Int.MinValue => (0, newRng)
+    case (n, newRng) if n < 0 => (-n, newRng)
+    case (n, newRng) => (n, newRng)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, newRng) = nonNegativeInt(rng)
+    (n / (Int.MaxValue.toDouble + 1), newRng)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, intRng) = nonNegativeInt(rng)
+    val (d, doubleRng) = double(intRng)
+    ((i, d), doubleRng)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), newRng) = intDouble(rng)
+    ((d, i), newRng)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(rng: RNG, count: Int, list: List[Int]): (List[Int], RNG) = {
+      count match {
+        case 0 => (list, rng)
+        case _ => {
+          val (n, newRng) = rng.nextInt
+          go(newRng, count - 1, n :: list)
+        }
+      }
+    }
+    go(rng, count, List())
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def nonNegativeEven(rng: RNG): (Int, RNG) =
+    map(nonNegativeInt)(i => i - i % 2)(rng)
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def doubleWithMap(rng: RNG): (Double, RNG) =
+    map(nonNegativeInt)(n => n / (Int.MaxValue.toDouble + 1))(rng)
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, rng1) = ra(rng)
+    val (b, rng2) = rb(rng1)
+    (f(a, b), rng2)
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    rng => {
+      val z = (Nil, rng)
+      fs.foldRight[(List[A], RNG)](z)((rand, acc) => {
+        val (aValue, newRng) = rand(acc._2)
+        (acc._1 :+ aValue, newRng)
+      })
+    }
+
+  def sequenceWithMap2[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    //    val z: Rand[List[A]] = rng => (Nil, rng)
+    //    fs.foldRight[Rand[List[A]]](z)((rand, acc) => map2(rand, acc)(_ :: _))
+    //    fs.foldRight[Rand[List[A]]](unit(Nil))((rand, acc) => map2(rand, acc)(_ :: _))
+    fs.foldRight[Rand[List[A]]](unit(Nil))(map2(_, _)(_ :: _))
+  }
+
+  def intsSequence(count: Int)(rng: RNG): (List[Int], RNG) = {
+    sequenceWithMap2(List.fill(count)(int))(rng)
+  }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (aValue, newRng) = f(rng)
+    g(aValue)(newRng)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+//    val greatestValidInt = ((Int.MaxValue / n) * n) - 1
+    flatMap(nonNegativeInt)(x => {
+      val mod = x % n
+      if ((x + (n-1) - mod) < 0) {
+        nonNegativeLessThan(n)
+      } else {
+        unit(mod)
+      }
+    })
+  }
+
+  def mapWithFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+    flatMap(s)(a => unit(f(a)))
+  }
+
+  def map2WithFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+  }
 }
 
-case class State[S,+A](run: S => (A, S)) {
+case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sys.error("todo")
+
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     sys.error("todo")
 }
 
 sealed trait Input
+
 case object Coin extends Input
+
 case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+}
+
+object test extends App {
+  private val rng = RNG.Simple(100002)
+  //  println(RNG.doubleWithMap(rng)._1)
+  //  println(RNG.double(rng)._1)
+  //  println(RNG.intsSequence(5)(rng)._1.mkString(", "))
+  //  println(RNG.map2(RNG.int, RNG.nonNegativeInt)((i, d) => s"$i: $d")(rng)._1)
+  //  println(RNG.sequence[Int](List(RNG.int, RNG.int))(rng)._1)
+  //  println(RNG.sequenceWithMap2[Int](List(RNG.int, RNG.int))(rng)._1)
+  println(RNG.nonNegativeLessThan(10)(rng))
 }
